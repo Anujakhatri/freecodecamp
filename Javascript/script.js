c583e4dfb9bb25da75b21c303cfb47f1043e8c48
Anuja khatri <khatrianuja20@gmail.com>
2025-10-30 22:55:56 +0545

    Js stuff

--- Javascript/script.js
+++ Javascript/script.js
@@ -3,13 +3,10 @@ let countEl=document.getElementById("count-el")
 
 console.log(countEl)
 let count=0
-
-
 function increment(){
     // console.log("clicked")
     count=count+1
     countEl.innerText=count
     // document.getElementById(count)
-
     // document.getElementById("count-el").innerText=count
 }
