14e859c0ed0a26a617c4e2ad1aa1c61607e126f8
Anuja khatri <khatrianuja20@gmail.com>
2025-11-01 00:20:57 +0545

    Basic js updated

--- Javascript/script.js
+++ Javascript/script.js
@@ -1,12 +1,16 @@
 // document.getElementById("count-el").innerText=5;
-let countEl=document.getElementById("count-el")
+let countEl = document.getElementById("count-el")
 
 console.log(countEl)
+
 let count=0
 function increment(){
     // console.log("clicked")
     count=count+1
-    countEl.innerText=count
     // document.getElementById(count)
     // document.getElementById("count-el").innerText=count
+    countEl.innerText = count
+}
+function save(){
+    console.log(count)
 }
